7890fccb48b5709227d3326e5013cb518a12ab15
neroux <neroux@users.noreply.github.com>
2017-08-27 14:18:55 +0200

    Taking agent from cookie than via messaging

--- extension/js/inject/content.js
+++ extension/js/inject/content.js
@@ -15,52 +15,42 @@
  * 0. You just DO WHAT THE FUCK YOU WANT TO.
  */
 
-chrome.runtime.sendMessage([
-  {action: 'settings.getEnabled'},
-  {action: 'settings.getJavascriptProtectionEnabled'},
-  {action: 'useragent.get'},
-  {action: 'exceptions.uriMatch', data: {uri: window.location.href}} // Make wildcard search in exceptions list
-], function(results) {
-  var enabled = results[0],
-    js_protection_enabled = results[1],
-    useragent = results[2],
-    uri_match = results[3];
-  var consoleMessage = function (message_text) {
-    if (typeof message_text === 'string') {
-      console.debug('%c [Random User-Agent] '+message_text+' ', 'background: transparent; color: rgba(0,0,0,0.45)');
+function getCookie(cookie)
+{
+  return document.cookie.split(';').reduce(function(prev, c) {
+    var arr = c.split('=');
+    return (arr[0].trim() === cookie) ? decodeURIComponent(arr[1]) : prev;
+  }, undefined);
+}
+
+var userAgent=getCookie('RUA_TRANSPORT_COOKIE');
+document.cookie = 'RUA_TRANSPORT_COOKIE=; expires=Thu, 01 Jan 1970 00:00:00 UTC;';
+
+if (userAgent)
+{
+  var injection_code = '(' + function(new_useragent) {
+    if (typeof window === 'object' && typeof window.navigator === 'object') {
+      navigator = Object.create(window.navigator);
+      Object.defineProperties(navigator, {
+        userAgent: {get: function() {return new_useragent;}},
+        appVersion: {get: function() {return new_useragent;}}
+      });
+      Object.defineProperty(window, 'navigator', {
+        value: navigator, configurable: false, enumerable: false, writable: false
+      });
     }
-  };
-  if (enabled === true) {
-    if (js_protection_enabled === true) {
-      if (typeof useragent === 'string' && useragent !== '') {
-        if (uri_match === false) {
-          consoleMessage('Use fake User-Agent: ' + useragent);
-          var injection_code = '(' + function(new_useragent) {
-                if (typeof window === 'object' && typeof window.navigator === 'object') {
-                  navigator = Object.create(window.navigator);
-                  Object.defineProperties(navigator, {
-                    userAgent:  {get: function() {return new_useragent;}},
-                    appVersion: {get: function() {return new_useragent;}}
-                  });
-                  Object.defineProperty(window, 'navigator', {
-                    value: navigator, configurable: false, enumerable: false, writable: false
-                  });
-                }
-              } + ')("' + useragent.replace(/([\"\'])/g, '\\$1') + '");';
-          var script = document.createElement('script');
-          script.textContent = injection_code;
-          document.documentElement.appendChild(script);
-          script.remove();
-          if (typeof navigator === 'object') {
-            Object.defineProperties(navigator, {
-              userAgent:  {get: function() {return useragent;}},
-              appVersion: {get: function() {return useragent;}}
-            });
-          }
-        }
-      }
-    } else {
-      consoleMessage('User-Agent JavaScript protection disabled!');
-    }
+  } + ')("' + userAgent.replace(/([\"\'])/g, '\\$1') + '");';
+
+  var script = document.createElement('script');
+  script.textContent = injection_code;
+  document.documentElement.appendChild(script);
+
+  script.remove();
+
+  if (typeof navigator === 'object') {
+    Object.defineProperties(navigator, {
+      userAgent: {get: function() {return userAgent;}},
+      appVersion: {get: function() {return userAgent;}}
+    });
   }
-});
+}
