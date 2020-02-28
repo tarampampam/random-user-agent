a96e8b760d39a1d52535b64bca5df825719fe63e
Paramtamtam <slashkot@gmail.com>
2020-02-28 17:28:23 +0500

    WIP

--- src/services/storages/chrome-storage.test.ts
+++ src/services/storages/chrome-storage.test.ts
@@ -1,19 +1,32 @@
 import ChromeStorage from "./chrome-storage";
 
-describe('chrome storage (using chrome.storage.local)', () => {
-  let chromeStorage = new ChromeStorage(false);
+/**
+ * Set `chrome.runtime.lastError` (just for a tests).
+ *
+ * @param {string | undefined} errorMessage
+ */
+const setLastRuntimeError = (errorMessage: string | undefined): void => {
+  let error = undefined;
 
-  it('can set without runtime error', () => {
+  if (typeof errorMessage === 'string') {
+    error = <chrome.runtime.LastError>{message: errorMessage};
+  }
+
+  (global as any).chrome = {...(global as any).chrome, ...{runtime: {lastError: error}}};
+};
+
+describe('chrome storage (using chrome.storage.LOCAL)', (): void => {
+  const chromeStorage = new ChromeStorage(false); // <-- `false` is important!
+
+  it('can set (without runtime error)', (): Promise<void> => {
     expect.assertions(1);
 
-    const set = jest.fn().mockImplementation((_, callback: Function) => {
+    const set = jest.fn().mockImplementation((_: any, callback: Function) => {
       callback();
     });
 
-    (global as any).chrome = {
-      storage: {local: {set}},
-      runtime: {lastError: undefined},
-    };
+    (global as any).chrome = {storage: {local: {set}}};
+    setLastRuntimeError(undefined);
 
     return chromeStorage.set('foo', {bar: 'baz'})
       .then(() => {
@@ -21,17 +34,19 @@ describe('chrome storage (using chrome.storage.local)', () => {
       })
   });
 
-  it('set with runtime error rejects promise', () => {
+  it('cannot set (with runtime error) - it rejects promise', (): Promise<void> => {
     expect.assertions(1);
 
-    const set = jest.fn().mockImplementation((_, callback: Function) => {
-      callback();
-    });
-
     (global as any).chrome = {
-      storage: {local: {set}},
-      runtime: {lastError: chrome.runtime.lastError = {message: 'test runtime error'}},
+      storage: {
+        local: {
+          set: jest.fn().mockImplementation((_: any, callback: Function) => {
+            callback();
+          })
+        }
+      },
     };
+    setLastRuntimeError('test runtime error');
 
     return chromeStorage.set('foo', {bar: 'baz'})
       .then(() => {
@@ -42,17 +57,19 @@ describe('chrome storage (using chrome.storage.local)', () => {
       })
   });
 
-  it('can get without runtime error', () => {
+  it('can get (without runtime error)', (): Promise<void> => {
     expect.assertions(1);
 
-    const get = jest.fn().mockImplementation((_, callback: Function) => {
-      callback({foo: {bar: 'baz'}});
-    });
-
     (global as any).chrome = {
-      storage: {local: {get}},
-      runtime: {lastError: undefined},
+      storage: {
+        local: {
+          get: jest.fn().mockImplementation((_: any, callback: Function) => {
+            callback({foo: {bar: 'baz'}});
+          })
+        }
+      }
     };
+    setLastRuntimeError(undefined);
 
     return chromeStorage.get('foo')
       .then((value) => {
@@ -60,38 +77,40 @@ describe('chrome storage (using chrome.storage.local)', () => {
       })
   });
 
-  it('get with runtime error rejects promise', () => {
+  it('cannot get with (with runtime error) - it rejects promise', (): Promise<void> => {
     expect.assertions(1);
 
-    const get = jest.fn().mockImplementation((_, callback: Function) => {
-      callback();
-    });
-
     (global as any).chrome = {
-      storage: {local: {get}},
-      runtime: {lastError: chrome.runtime.lastError = {message: 'test runtime error'}},
+      storage: {
+        local: {
+          get: jest.fn().mockImplementation((_: any, callback: Function) => callback())
+        }
+      }
     };
+    setLastRuntimeError('test runtime error');
 
     return chromeStorage.get('foo')
       .then(() => {
-        throw new Error('.then must not be called');
+        throw new Error('.then must not be called')
       })
       .catch((error: Error) => {
         expect(error.message).toEqual('test runtime error');
       })
   });
 
-  it('get without runtime error but without required key rejects promise', () => {
+  it('cannot get (without runtime error, but without required key) - it rejects promise', (): Promise<void> => {
     expect.assertions(1);
 
-    const get = jest.fn().mockImplementation((_, callback: Function) => {
-      callback({blah: {bar: 'baz'}});
-    });
-
     (global as any).chrome = {
-      storage: {local: {get}},
-      runtime: {lastError: undefined},
+      storage: {
+        local: {
+          get: jest.fn().mockImplementation((_: any, callback: Function) => {
+            callback({blah: {bar: 'baz'}});
+          })
+        }
+      }
     };
+    setLastRuntimeError(undefined);
 
     return chromeStorage.get('foo')
       .then(() => {
@@ -102,17 +121,15 @@ describe('chrome storage (using chrome.storage.local)', () => {
       })
   });
 
-  it('can clear without runtime error', () => {
+  it('can clear (without runtime error)', (): Promise<void> => {
     expect.assertions(1);
 
     const clear = jest.fn().mockImplementation((callback: Function) => {
       callback();
     });
 
-    (global as any).chrome = {
-      storage: {local: {clear}},
-      runtime: {lastError: undefined},
-    };
+    (global as any).chrome = {storage: {local: {clear}}};
+    setLastRuntimeError(undefined);
 
     return chromeStorage.clear()
       .then(() => {
@@ -120,17 +137,19 @@ describe('chrome storage (using chrome.storage.local)', () => {
       })
   });
 
-  it('clear with runtime error rejects promise', () => {
+  it('cannot clear (with runtime error) - it rejects promise', (): Promise<void> => {
     expect.assertions(1);
 
-    const clear = jest.fn().mockImplementation((callback: Function) => {
-      callback();
-    });
-
     (global as any).chrome = {
-      storage: {local: {clear}},
-      runtime: {lastError: chrome.runtime.lastError = {message: 'test runtime error'}},
+      storage: {
+        local: {
+          clear: jest.fn().mockImplementation((callback: Function) => {
+            callback();
+          })
+        }
+      },
     };
+    setLastRuntimeError('test runtime error');
 
     return chromeStorage.clear()
       .then(() => {
@@ -138,6 +157,262 @@ describe('chrome storage (using chrome.storage.local)', () => {
       })
       .catch((error: Error) => {
         expect(error.message).toEqual('test runtime error');
+      })
+  });
+});
+
+describe('chrome storage (using chrome.storage.SYNC)', (): void => {
+  const chromeStorage = new ChromeStorage(true); // <-- `true` is important!
+
+  it('can set (without runtime error)', (): Promise<void> => {
+    expect.assertions(1);
+
+    const set = jest.fn().mockImplementation((_: any, callback: Function) => {
+      callback();
+    });
+
+    (global as any).chrome = {storage: {sync: {set}}};
+    setLastRuntimeError(undefined);
+
+    return chromeStorage.set('foo', {bar: 'baz'})
+      .then(() => {
+        expect(set).toHaveBeenCalledWith({foo: {bar: 'baz'}}, expect.any(Function));
+      })
+  });
+
+  it('can set (with runtime error) - it uses local storage as fallback', (): Promise<void> => {
+    expect.assertions(2);
+
+    const syncSet = jest.fn().mockImplementation((_: any, callback: Function) => {
+      callback({bar: 'baz'});
+    });
+
+    const localSet = jest.fn().mockImplementation((_: any, callback: Function) => {
+      setLastRuntimeError(undefined);
+
+      callback({bar: 'baz'});
+    });
+
+    (global as any).chrome = {
+      storage: {
+        sync: {
+          set: syncSet
+        },
+        local: {
+          set: localSet
+        }
+      },
+    };
+    setLastRuntimeError('test runtime error');
+
+    return chromeStorage.set('foo', {bar: 'baz'})
+      .then(() => {
+        expect(syncSet).toHaveBeenCalledWith({foo: {bar: 'baz'}}, expect.any(Function));
+        expect(localSet).toHaveBeenCalledWith({foo: {bar: 'baz'}}, expect.any(Function));
+      })
+      .catch((error: Error) => {
+        throw new Error('.catch must not be called: ' + error.message);
+      })
+  });
+
+  it('cannot can set (with runtime error on local and sync storage) - it rejects promise', (): Promise<void> => {
+    expect.assertions(1);
+
+    (global as any).chrome = {
+      storage: {
+        sync: {
+          set: jest.fn().mockImplementation((_: any, callback: Function) => {
+            callback();
+          })
+        },
+        local: {
+          set: jest.fn().mockImplementation((_: any, callback: Function) => {
+            setLastRuntimeError('another runtime error');
+            callback();
+          })
+        }
+      },
+    };
+    setLastRuntimeError('test runtime error');
+
+    return chromeStorage.set('foo', {bar: 'baz'})
+      .then(() => {
+        throw new Error('.then must not be called');
+      })
+      .catch((error: Error) => {
+        expect(error.message).toEqual('another runtime error');
+      })
+  });
+
+  it('can get (without runtime error)', (): Promise<void> => {
+    expect.assertions(1);
+
+    (global as any).chrome = {
+      storage: {
+        sync: {
+          get: jest.fn().mockImplementation((_: any, callback: Function) => {
+            callback({foo: {bar: 'baz'}});
+          })
+        }
+      },
+    };
+    setLastRuntimeError(undefined);
+
+    return chromeStorage.get('foo')
+      .then((value) => {
+        expect(value).toEqual({bar: 'baz'});
+      })
+  });
+
+  it('can get (with runtime error) - it uses local storage as fallback', (): Promise<void> => {
+    expect.assertions(1);
+
+    (global as any).chrome = {
+      storage: {
+        sync: {
+          get: jest.fn().mockImplementation((_: any, callback: Function) => {
+            callback();
+          })
+        }, local: {
+          get: jest.fn().mockImplementation((_: any, callback: Function) => {
+            setLastRuntimeError(undefined);
+
+            callback({foo: {bar: 'baz'}});
+          })
+        }
+      },
+    };
+    setLastRuntimeError('test runtime error');
+
+    return chromeStorage.get('foo')
+      .then((value) => {
+        expect(value).toEqual({bar: 'baz'});
+      })
+      .catch((error: Error) => {
+        throw new Error('.catch must not be called: ' + error.message);
+      })
+  });
+
+  it('cannot get (with runtime error on local and sync storage) - it rejects promise', (): Promise<void> => {
+    expect.assertions(1);
+
+    (global as any).chrome = {
+      storage: {
+        sync: {
+          get: jest.fn().mockImplementation((_: any, callback: Function) => {
+            callback();
+          })
+        },
+        local: {
+          get: jest.fn().mockImplementation((_: any, callback: Function) => {
+            callback({foo: {bar: 'baz'}});
+          })
+        }
+      },
+    };
+    setLastRuntimeError('test runtime error');
+
+    return chromeStorage.get('foo')
+      .then(() => {
+        throw new Error('.then must not be called');
+      })
+      .catch((error: Error) => {
+        expect(error.message).toEqual('test runtime error');
+      })
+  });
+
+  it('cannot get (without runtime error, but without required key) - it rejects promise', (): Promise<void> => {
+    expect.assertions(1);
+
+    (global as any).chrome = {
+      storage: {
+        sync: {
+          get: jest.fn().mockImplementation((_: any, callback: Function) => {
+            callback();
+          })
+        },
+        local: {
+          get: jest.fn().mockImplementation((_: any, callback: Function) => {
+            setLastRuntimeError(undefined);
+
+            callback({blah: {bar: 'baz'}});
+          })
+        }
+      }
+    };
+    setLastRuntimeError('test runtime error');
+
+    return chromeStorage.get('foo')
+      .then(() => {
+        throw new Error('.then must not be called');
+      })
+      .catch((error: Error) => {
+        expect(error.message).toEqual('Local storage does not contains expected data');
+      })
+  });
+
+  it('can clear (without runtime error)', (): Promise<void> => {
+    expect.assertions(1);
+
+    const clear = jest.fn().mockImplementation((callback: Function) => {
+      callback();
+    });
+
+    (global as any).chrome = {storage: {sync: {clear}}};
+    setLastRuntimeError(undefined);
+
+    return chromeStorage.clear()
+      .then(() => {
+        expect(clear).toHaveBeenCalledWith(expect.any(Function));
+      })
+  });
+
+  it('cannot clear (with runtime error) - it rejects promise', (): Promise<void> => {
+    expect.assertions(1);
+
+    (global as any).chrome = {
+      storage: {
+        sync: {
+          clear: jest.fn().mockImplementation((callback: Function) => callback())
+        }, local: {
+          clear: jest.fn().mockImplementation((callback: Function) => callback())
+        }
+      }
+    };
+    setLastRuntimeError('test runtime error');
+
+    return chromeStorage.clear()
+      .then(() => {
+        throw new Error('.then must not be called');
+      })
+      .catch((error: Error) => {
+        expect(error.message).toEqual('test runtime error');
+      })
+  });
+
+  it('can clear (with runtime error) - it uses local storage as fallback', (): Promise<void> => {
+    expect.assertions(2);
+
+    const localClear = jest.fn().mockImplementation((callback: Function) => {
+      setLastRuntimeError(undefined);
+
+      callback();
+    });
+
+    const syncClear = jest.fn().mockImplementation((callback: Function) => {
+      callback();
+    });
+
+    (global as any).chrome = {storage: {sync: {clear: syncClear}, local: {clear: localClear}}};
+    setLastRuntimeError('test runtime error');
+
+    return chromeStorage.clear()
+      .then(() => {
+        expect(localClear).toHaveBeenCalledWith(expect.any(Function));
+        expect(syncClear).toHaveBeenCalledWith(expect.any(Function));
+      })
+      .catch((error: Error) => {
+        throw new Error('.catch must not be called: ' + error.message);
       })
   });
 });
--- src/services/storages/chrome-storage.ts
+++ src/services/storages/chrome-storage.ts
@@ -1,6 +1,9 @@
 /**
  * This storage implementation uses `chrome.storage.(sync|local)` storage's.
  *
+ * If "prefer sync storage" is set to `true`, and while working with `chrome.storage.sync` storage error happened - as
+ * fallback will be used `chrome.storage.local` storage.
+ *
  * Requires Chrome 34 and above.
  *
  * @link <https://developer.chrome.com/extensions/storage>
@@ -22,28 +25,28 @@ export default class ChromeStorage implements Services.Storage {
       ? chrome.storage.sync
       : chrome.storage.local;
 
-    return new Promise((resolve: Function, reject: Function): void => {
+    return new Promise((resolve: () => void, reject: (_: Error) => void): void => {
       // execute `clear` method for `*.local` or `*.sync` storage
-      storage.clear(() => {
-        let error = chrome.runtime.lastError;
+      storage.clear((): void => {
+        let storageError = chrome.runtime.lastError;
 
         // if some error happened
-        if (error) {
+        if (storageError) {
           // `*.local` storage is used - in this case we have no fallback
           if (!this.preferSyncStorage) {
-            reject(new Error(error.message));
+            reject(new Error(storageError.message));
 
             return;
           }
 
           // repeat `clear()` action with `*.local` storage
           chrome.storage.local.clear((): void => {
-            let error = chrome.runtime.lastError;
+            let localStorageError = chrome.runtime.lastError;
 
             // check for error again
-            if (error) {
+            if (localStorageError) {
               // and if any error happened - call `reject` action
-              reject(new Error(error.message));
+              reject(new Error(localStorageError.message));
 
               return;
             }
@@ -57,12 +60,12 @@ export default class ChromeStorage implements Services.Storage {
     });
   }
 
-  get(key: string): Promise<{ [p: string]: any }> {
+  get(key: string): Promise<{ [key: string]: any }> {
     const storage = this.preferSyncStorage
       ? chrome.storage.sync
       : chrome.storage.local;
 
-    return new Promise((resolve: Function, reject: Function) => {
+    return new Promise((resolve: ({}: { [key: string]: any }) => void, reject: (_: Error) => void): void => {
       storage.get(key, (items): void => {
         let error = chrome.runtime.lastError;
 
@@ -76,7 +79,7 @@ export default class ChromeStorage implements Services.Storage {
           }
 
           // try to repeat this action with `*.local` storage
-          chrome.storage.local.get((items) => {
+          chrome.storage.local.get(key, (items) => {
             let error = chrome.runtime.lastError;
 
             // check for error again
@@ -87,7 +90,7 @@ export default class ChromeStorage implements Services.Storage {
               return;
             }
 
-            if (!items.hasOwnProperty(key)) {
+            if (!items[key]) {
               reject(new Error('Local storage does not contains expected data'));
 
               return;
@@ -97,7 +100,7 @@ export default class ChromeStorage implements Services.Storage {
           });
         }
 
-        if (!items.hasOwnProperty(key)) {
+        if (!items[key]) {
           reject(new Error('Storage does not contains expected data'));
 
           return;
@@ -108,15 +111,15 @@ export default class ChromeStorage implements Services.Storage {
     });
   }
 
-  set(key: string, value: { [p: string]: any }): Promise<void> {
+  set(key: string, value: { [key: string]: any }): Promise<void> {
     const storage = this.preferSyncStorage
       ? chrome.storage.sync
       : chrome.storage.local;
 
     const data = {[key]: value};
 
-    return new Promise((resolve: Function, reject: Function) => {
-      storage.set(data, () => {
+    return new Promise((resolve: () => void, reject: (_: Error) => void): void => {
+      storage.set(data, (): void => {
         let error = chrome.runtime.lastError;
 
         // if some error happened
@@ -129,7 +132,7 @@ export default class ChromeStorage implements Services.Storage {
           }
 
           // try to repeat this action with `*.local` storage
-          chrome.storage.local.set(data, () => {
+          chrome.storage.local.set(data, (): void => {
             let error = chrome.runtime.lastError;
 
             // check for error again
