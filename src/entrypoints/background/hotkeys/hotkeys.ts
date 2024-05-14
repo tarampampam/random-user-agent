ab742a36cf08e51e8971f488c20d5478d95dd2fc
Pаramtamtām <7326800+tarampampam@users.noreply.github.com>
2024-05-14 13:47:47 +0400

    fix: `chrome.commands` API may be unavailable for some reasons, an additional check for this case was added (#578)

--- src/entrypoints/background/hotkeys/hotkeys.ts
+++ src/entrypoints/background/hotkeys/hotkeys.ts
@@ -1,4 +1,8 @@
 export default function (handlers: { renewUserAgent: () => Promise<void> | void }) {
+  if (!chrome?.commands?.onCommand) {
+    return
+  }
+
   chrome.commands.onCommand.addListener(async (command) => {
     switch (command) {
       case 'renew-useragent': // command name, defined in manifest.json
